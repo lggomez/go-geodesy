46fc7e9c2375dfe8ad50d924b56061221a575278
Luis Gabriel Gomez <luis.gg1990@gmail.com>
2021-09-23 13:19:34 -0300

    vincenty: Perform correct squared trig functions

--- distance/vincenty.go
+++ distance/vincenty.go
@@ -1,18 +1,18 @@
 package distance
 
 import (
-	"math"
-
 	"github.com/lggomez/go-geodesy"
 	"github.com/lggomez/go-geodesy/ellipsoids"
+	"github.com/lggomez/go-geodesy/math"
+	stdMath "math"
 )
 
 const (
 	defaultAccuracy float64 = 1e-12 // approximates to 0.06 mm
 	maxIterations   int     = 50
 
-	fullAngleRad = 2 * math.Pi
-	radConversionFactor = 180/math.Pi
+	fullAngleRad = 2 * stdMath.Pi
+	radConversionFactor = 180/ stdMath.Pi
 )
 
 /*
@@ -44,7 +44,7 @@ func VincentyInverse(p1, p2 geodesy.Point, accuracy float64, calculateAzimuth bo
 
 	if p1.IsAntipode(p2) {
 		// Antipodes are non-convergent
-		return math.NaN(), math.NaN(), math.NaN()
+		return stdMath.NaN(), stdMath.NaN(), stdMath.NaN()
 	}
 
 	ε := defaultAccuracy
@@ -56,68 +56,72 @@ func VincentyInverse(p1, p2 geodesy.Point, accuracy float64, calculateAzimuth bo
 	a := ellipsoids.WGS84_SEMI_MAJOR_AXIS
 	b := ellipsoids.WGS84_SEMI_MINOR_AXIS
 	ƒ := ellipsoids.WGS84_FLATTENING
-	u1 := math.Atan((1 - ƒ) * math.Tan(p1.LatRadians())) // Reduced latitude for p1
-	u2 := math.Atan((1 - ƒ) * math.Tan(p2.LatRadians())) // Reduced latitude for p2
-	L := p2.LonRadians() - p1.LonRadians()               // Difference in longitude
-	λ := L                                               // Difference in longitude of the points on the auxiliary sphere
+	u1 := stdMath.Atan((1 - ƒ) * stdMath.Tan(p1.LatRadians())) // Reduced latitude for p1
+	u2 := stdMath.Atan((1 - ƒ) * stdMath.Tan(p2.LatRadians())) // Reduced latitude for p2
+	L := p2.LonRadians() - p1.LonRadians()                     // Difference in longitude
+	λ := L                                                     // Difference in longitude of the points on the auxiliary sphere
 	λ_prev := float64(0)
 	f16Frac := ƒ / 16
-	sinu1, cosu1 := math.Sincos(u1)
-	sinu2, cosu2 := math.Sincos(u2)
+	sinu1, cosu1 := stdMath.Sincos(u1)
+	sinu2, cosu2 := stdMath.Sincos(u2)
 
 	// Loop variables
 	cos2α := float64(0)
 	sinσ, cosσ := float64(0), float64(0)
 	sinλ, cosλ := float64(0), float64(0)
 	cos2σₘ := float64(0)
+	val_2σₘ := float64(0)
 	σ := float64(0)
-	sinλ, cosλ = math.Sincos(λ)
+	sinλ, cosλ = stdMath.Sincos(λ)
 
 	// Perform iterative evaluation of λ until it either converges to ε or reaches the maximum amount of iterations
-	for i := 0; math.Abs(λ-λ_prev) > ε; i++ {
+	for i := 0; stdMath.Abs(λ-λ_prev) > ε; i++ {
 		// Test for divergence on max iterations
 		if i > maxIterations {
-			return math.NaN(), math.NaN(), math.NaN()
+			return stdMath.NaN(), stdMath.NaN(), stdMath.NaN()
 		}
 
-		sinσ = math.Sqrt((cosu2*sinλ)*(cosu2*sinλ) +
+		sinσ = stdMath.Sqrt((cosu2*sinλ)*(cosu2*sinλ) +
 			((cosu1*sinu2)-(sinu1*cosu2*cosλ))*((cosu1*sinu2)-(sinu1*cosu2*cosλ)))
 		if sinσ == 0 {
 			// Indeterminate sinα; It represents an end point coincident with,
 			// or diametrically opposed to, the start point.
-			return math.NaN(), math.NaN(), math.NaN()
+			return stdMath.NaN(), stdMath.NaN(), stdMath.NaN()
 		}
 
 		cosσ = (sinu1 * sinu2) + (cosu1 * cosu2 * cosλ)
-		σ = math.Atan2(sinσ, cosσ) // Angular separation between points
+		σ = stdMath.Atan2(sinσ, cosσ) // Angular separation between points
 
 		sinα := (cosu1 * cosu2 * sinλ) / sinσ
-
+		α := stdMath.Asin(sinα)
 		cos2σₘ = float64(0)
+		val_2σₘ = float64(0)
 		C := float64(0)
 		// Distances through the equator yield C = 0 and cos2σₘ is not used,
 		// so calculate if points do not fall on it
 		if (p1.Lat() != 0) && (p2.Lat() != 0) {
-			cos2α = 1 - (sinα * sinα)
+			cos2α = 1 - math.Sin2(α)
 			cos2σₘ = cosσ - ((2 * sinu1 * sinu2) / cos2α)
+			val_2σₘ = stdMath.Acos(cos2σₘ)
 			C = f16Frac * cos2α * (4 + ƒ*(4-3*cos2α))
 		}
 
 		λ_prev = λ
-		λ = L + (1-C)*ƒ*sinα*(σ+C*sinσ*(cos2σₘ+C*cosσ*(-1+2*cos2σₘ*cos2σₘ)))
+		λ = L + (1-C)*ƒ*sinα *
+			(σ+C*sinσ*(cos2σₘ+C*cosσ*(-1+2*math.Cos2(val_2σₘ))))
 
-		sinλ, cosλ = math.Sincos(λ)
+		sinλ, cosλ = stdMath.Sincos(λ)
 	}
 
 	// Setup return variables
-	α1 := math.NaN()
-	α2 := math.NaN()
+	α1 := stdMath.NaN()
+	α2 := stdMath.NaN()
 	d := float64(0)
 
 	bSquared := b * b
 	uSquared := cos2α * (((a * a) - bSquared) / bSquared)
 
-	A := 1 + (uSquared/16384)*(4096+uSquared*(-768+uSquared*(320-175*uSquared)))
+	A := 1 + (uSquared/16384) * (4096+uSquared*(-768+uSquared*(320-175*uSquared)))
 	B := (uSquared / 1024) * (256 + uSquared*(-128+uSquared*(74-47*uSquared)))
 	Δσ := B * sinσ *
 		(cos2σₘ + (B/4)*(cosσ*(-1+2*(cos2σₘ*cos2σₘ))-
@@ -129,12 +133,12 @@ func VincentyInverse(p1, p2 geodesy.Point, accuracy float64, calculateAzimuth bo
 	if calculateAzimuth {
 		numα1 := cosu2*sinλ
 		denomα1 := (cosu1*sinu2)-(sinu1*cosu2*cosλ)
-		α1 = quadrantRadToDegree(math.Atan2(numα1, denomα1))
+		α1 = quadrantRadToDegree(stdMath.Atan2(numα1, denomα1))
 
 		numα2 := cosu1*sinλ
 		denomα2 := (-sinu1*cosu2)+(cosu1*sinu2*cosλ)
-		α2 = quadrantRadToDegree(math.Atan2(numα2, denomα2))
-		α2 = math.Mod(α2 + 180, 360) // Normalize degree to north meridian as origin vector
+		α2 = quadrantRadToDegree(stdMath.Atan2(numα2, denomα2))
+		α2 = stdMath.Mod(α2 + 180, 360) // Normalize degree to north meridian as origin vector
 	}
 
 	return d, α1, α2
---
+++ math/trig.go
@@ -0,0 +1,23 @@
+package math
+
+import "math"
+
+// Sin2 calculates the Taylor expansion of sin²x up to the 5th term,
+// allowing for similar precision for x in the approximate range of (-2;2)
+func Sin2(x float64) float64 {
+	return math.Pow(x, 2) -
+		math.Pow(x, 4)/3 +
+		(2*math.Pow(x, 6))/45 -
+		math.Pow(x, 8)/315 +
+		(2*math.Pow(x, 10))/14175
+}
+
+// Cos2 calculates the Taylor expansion of cos²x up to the 5th term,
+// allowing for similar precision for x in the approximate range of (-2;2)
+func Cos2(x float64) float64 {
+	return 1 - math.Pow(x, 2) +
+		math.Pow(x, 4)/3 -
+		(2*math.Pow(x, 6))/45 +
+		math.Pow(x, 8)/315 -
+		(2*math.Pow(x, 10))/14175
+}
